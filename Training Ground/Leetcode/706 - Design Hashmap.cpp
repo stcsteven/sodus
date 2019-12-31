7c0c6b7300c26546e236f3ffe70458d2c3cac4bb
Steven Candra <steven.candra5@gmail.com>
2019-12-31 23:47:02 +0700

    add leetcode problems

---
+++ Training Ground/Leetcode/706 - Design Hashmap.cpp
@@ -0,0 +1,104 @@
+class Node{    
+public:
+    int key, value;
+    Node* next;
+
+    Node(int key, int value){
+        this->key = key;
+        this->value = value;
+        this->next = NULL;
+    }
+    
+    void updateValue(int value){
+        this->value = value;
+    }
+};
+
+class MyHashMap {
+private:
+    Node* head;
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        head = NULL;
+    }
+    
+    bool updateHead(int key, int value){
+        if(head==NULL){
+            head = new Node(key, value);
+            return true;
+        } else{
+            if(head->key == key){
+                head->value = value;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        if(updateHead(key, value)) return;
+        Node* now = head;
+        bool is_new = true;
+        while(now->next!=NULL){
+            now = now->next;
+            if(now->key==key){
+                is_new = false;
+                break;
+            }
+        } 
+        if (is_new)
+            now->next = new Node(key, value);
+        else
+            now->value = value;
+        return;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        Node* now = head;
+        while(now!=NULL){
+            if(now->key==key)
+                return now->value;
+            now = now->next;
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        if(keyAtHead(key)) return;
+        if(keyAtBody(key)) return;
+        return;
+    }
+    
+    bool keyAtHead(int key){
+        if(head==NULL) return true;
+        if(head->key == key){
+            head = head->next;
+            return true;        
+        }
+        return false;
+    }
+    
+    bool keyAtBody(int key){
+        Node* now = head;
+        while(now!=NULL && now->next!=NULL){
+            if(now->next->key==key){
+                now->next = now->next->next;
+                return true;
+            }
+            now = now->next;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
---
+++ Training Ground/Leetcode/92 - Reversed LinkedList.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findAddr(ListNode* head, int counter){
+        ListNode* now = head;
+        while(counter--)
+            now = now->next;
+        return now;
+    }
+    
+    ListNode* addNode(ListNode* now_head, int value){
+        ListNode* new_node = new ListNode(value);
+        
+        if(now_head==NULL)
+            now_head = new_node;
+        else{
+            ListNode* temp = new_node;
+            new_node->next = now_head;
+            now_head = temp;
+        }
+        return now_head;
+    }
+    
+    ListNode* reverse(ListNode* prevHead, int counter){
+        ListNode* new_head = NULL; 
+        ListNode* now = prevHead;
+        while(counter--){
+            new_head = addNode(new_head, now->val);
+            now = now->next;
+        }
+        return new_head;
+    }
+    
+    ListNode* lastOf(ListNode* head){
+        ListNode* now = head;
+        while(now->next!=NULL)
+            now = now->next;
+        return now;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if(n<=m) 
+            return head;
+        ListNode* new_head;
+        ListNode* normal_tail = findAddr(head, n);
+        ListNode* reversed_list = reverse(findAddr(head, m-1), n-m+1);
+        ListNode* last_reversed_node = lastOf(reversed_list);
+        if(m==1){
+            last_reversed_node->next = normal_tail;
+            new_head = reversed_list;
+        } else{
+            ListNode* normal_head = findAddr(head, m-2);
+            normal_head->next = reversed_list;
+            last_reversed_node->next = normal_tail;
+            new_head = head;
+        }
+        return new_head;
+    }
+};
