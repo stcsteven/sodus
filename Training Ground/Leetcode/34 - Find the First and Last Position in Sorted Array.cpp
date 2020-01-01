e06e13ee27aab400a7638a6bd635dd3535db6dba
Steven Candra <steven.candra5@gmail.com>
2020-01-01 21:17:57 +0700

    add lower bound

---
+++ Training Ground/Leetcode/34 - Find the First and Last Position in Sorted Array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    /*
+        Solusinya adalah implementasi LowerBound + UpperBound pakai BinarySearch 
+        BinarySearch has O(logN) complexity since the counter is done by using division with 2
+        
+        Important Question => kenapa rangenya dari 0 sampe nums.size() bukan sampe nums.size()-1
+        Nb. http://www.cplusplus.com/reference/algorithm/lower_bound/
+
+        Consider the following example -> array = [1,2]; target = 2
+        By implementing the range from 0 to array.size(), the mid value will be 1 instead of 0 (when u use array.size()-1) 
+    */
+
+    int lowerBound(vector<int>& nums, int target){
+        int start = 0, ans=-1;
+        int end = nums.size(); 
+        while(start<end){
+            int mid = start + ((end-start)>>1);
+            if(nums[mid]==target) ans = mid;
+            if(nums[mid]<target) start = mid+1;
+            else end = mid;
+        }
+        return ans;
+    }
+    
+    int upperBound(vector<int>& nums, int target){
+        int start = 0, ans=-1;
+        int end = nums.size(); 
+        while(start<end){
+            int mid = start + ((end-start)>>1);
+            if(nums[mid]==target) ans = mid;
+            if(nums[mid]<=target) start = mid+1;
+            else end = mid;
+        }
+        return ans;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        ans.push_back(lowerBound(nums, target));
+        ans.push_back(upperBound(nums, target));
+        return ans;
+    }
+};
